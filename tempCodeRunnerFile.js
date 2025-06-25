aa3db7efe9aa68bf4a55a21086fb7a068ffeb91e
KRISHNA GUPTA <krishnaa941255@gmail.com>
2025-06-25 19:06:22 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+document.getElementById("img1").setAttribute("src",image1)
+// var randomNumber2=Math.random()*6;
+// randomNumber2=Math.floor(randomNumber2)+1;
+// var image2="./images/dice"+randomNumber2+".png";
+// document.getElementById("img2").setAttribute("src",image2)
